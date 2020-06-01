69113156b010c2599907d9eb30d7742dadecba7d
Luciano Verni <lucianoverni@gmail.com>
2020-06-01 02:46:39 +0200

    new update

--- webscrape_bids.py
+++ webscrape_bids.py
@@ -32,52 +32,50 @@ class BidScrape():
         driver = self.driver
         WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,  "//*[@id='ctl00_CPH1_lnkVolver']"))).click()
        
-    def page_jump(self):
+        
+    def first_page_jump(self):
         time.sleep(5)
         WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.LINK_TEXT, "..."))).click()
+        
+    def page_jump(self, idx):
         for scraped_pages in range(self.page_counter - 1):
             time.sleep(5)
-            WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='ctl00_CPH1_GridListaPliegos']/tbody/tr[12]/td/table/tbody/tr/td[12]/a"))).click()
+            WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, f"//*[@id='ctl00_CPH1_GridListaPliegos']/tbody/tr[{idx[0]}]/td/table/tbody/tr/td[{idx[1]}]/a"))).click()
             
-    def tab_jump(self, tab_lst):
-        self.wait()
-        tab_lst[(self.tab_counter % 10)].click()
-          
-    def wait(self):
-        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='ctl00_CPH1_GridListaPliegos']/tbody/tr[12]/td/table/tbody/tr/td[12]/a")))
-
-    def tab_lst(self):
-        self.wait()
-        tab_grid = self.driver.find_element_by_class_name("pagination-gv")
-        return 
+    def tab_jump(self, tab_idx):
+        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.LINK_TEXT, f"{tab_idx}"))).click()
         
     def scrape(self):
         driver = self.driver
-        self.page_jump()
-    
-        # if (self.tab_counter % 10) > 0:
-  
+        # self.first_page_jump()
+        self.page_jump([12, 12])
+        if (self.tab_counter % 10) > 0:
+            self.tab_jump(self.tab_counter+1)
             
-    
-    
-        #SET PAGE_COUNTER AND TAB_COUNTER CHANGES
-        
-        
+        for n in range(20):
+            for row in range(2, 12):
+                row = format(row, "02d")
                 
-        
-        #         for row in range(2, 12):
-        #             row = format(row, "02d")
-        #             time.sleep(10)
-        #             WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,  f"//*[@id='ctl00_CPH1_GridListaPliegos_ctl{row}_lnkNumeroProceso']"))).click()
-        #             self.extract()
-        #             self.row_counter += 1
-        #             print("Row " + str(self.row_counter) + " scraped")
-                    
-        #         self.tab_counter += 1
-        #         print("Tab " + str(self.tab_counter) + " scraped")
+                WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR,  f"#ctl00_CPH1_GridListaPliegos_ctl02_lnkNumeroProceso a"))).click()
+                
+                self.extract()
+                self.row_counter += 1
+                print("Row " + str(self.row_counter) + " scraped")
+            
+            if (self.tab_counter % 10) == 0:
+                self.page_jump([11, 12])
+                print("Page" + str(self.page_counter) + " scraped")
+                self.page_counter += 1
+                print("Tab " + str(self.tab_counter) + " scraped")
+                self.tab_counter += 1
+            else:
+                self.tab_jump(self.tab_counter + 1)
+                print("Tab " + str(self.tab_counter) + " scraped")
+                self.tab_counter += 1
+            
            
         #     self.page_counter += 1
-        #     print("Page" + str(self.page_counter) + " scraped")
+        #     
             
         # except (TimeoutException, NoSuchElementException) as error:
         #     print("finalizing due to error...")
@@ -91,6 +89,6 @@ class BidScrape():
     
 
         
-compras_ar = BidScrape("https://comprar.gob.ar/BuscarAvanzado.aspx", 2, 12, 113)
+compras_ar = BidScrape("https://comprar.gob.ar/BuscarAvanzado.aspx", 0, 1, 0)
 compras_ar.query_search()
 compras_ar.scrape()
