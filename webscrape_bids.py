2740589b5cbdae61dacc62b83b6bae50d1b54ba7
Luciano Verni <lucianoverni@gmail.com>
2020-06-02 02:45:07 +0200

    extraction process finalized

--- webscrape_bids.py
+++ webscrape_bids.py
@@ -6,86 +6,36 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import NoSuchElementException
+from bs4 import BeautifulSoup
+import requests
 import re
 import unittest
 import time
 
 
+link = "https://comprar.gob.ar/PLIEGO/VistaPreviaPliegoCiudadano.aspx?qs=BQoBkoMoEhzx8zvkTDdwTXg17CFDfb9gjz|hkhikVuyhUW1N8Tfv5zjexUkKfyHHxcg3AZulJorys/QkdmWV5fw7Z5QjQLd2M0P5kfWqNWehoUyNMe16yC4Pdh8gFi94uVNDZzV4kXjLgb2wbcE23HGRBQNBx|acgVvZv5d3Ya6JgsxUdFvBNQ=="
 
-class BidScrape():
 
-    def __init__(self, url, page_counter, tab_counter, row_counter):
-        self.driver = webdriver.Chrome("\webdrivers\chromedriver.exe")
-        self.driver.set_script_timeout(20)
-        accept_next_alert = True
-        delay = 3
-        self.driver.get(url)
-        self.page_counter = page_counter
-        self.tab_counter = tab_counter 
-        self.row_counter = row_counter
-        
-    def wait_for_id(self, element, condition):
-        driver = self.driver
-        if element == "id":
-            driver = self.driver
-        WebDriverWait(driver, 20).until(
-            lambda driver: driver.find_element_by_id(condition))
-        if element == "class":
-            driver = self.driver
-        WebDriverWait(driver, 20).until(
-            lambda driver: driver.find_element_by_class_name(condition))
-    
-    def query_search(self):
-        driver = self.driver
-        status = Select(driver.find_element_by_id("ctl00_CPH1_ddlEstadoProceso"))
-        status.options[1].click()
-        driver.find_element_by_id("ctl00_CPH1_btnListarPliegoAvanzado").click()
-        
-    def extract(self):
-        driver = self.driver
-        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,  "//*[@id='ctl00_CPH1_lnkVolver']"))).click()
-       
-    def tab_jump(self):
-        tab_jumps = int((self.tab_counter - (self.tab_counter % 10)) / 10) 
-        driver = self.driver
-        for jump in tab_jumps:
-            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,  f"//*[@id='ctl00_CPH1_GridListaPliegos']/tbody/tr[11]/td/table/tbody/tr/td[{12}]/a"))).click()
-        
-    def scrape(self):
-        if str((self.tab_counter % 10) + 1) < 3:
-            next_tab = "3"
-        next_tab = str((self.tab_counter % 10) + 1)
-        driver = self.driver       
-        try:
-            for tab in range(next_tab, 10):
-                WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,  f"//*[@id='ctl00_CPH1_GridListaPliegos']/tbody/tr[11]/td/table/tbody/tr/td[{tab}]/a"))).click()
-                
-                for row in range(2, 12):
-                    row = format(row, "02d")
-                    time.sleep(10)
-                    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH,  f"//*[@id='ctl00_CPH1_GridListaPliegos_ctl{row}_lnkNumeroProceso']"))).click()
-                    self.extract()
-                    self.row_counter += 1
-                    print("Row " + str(self.row_counter) + " scraped")
-                    
-                self.tab_counter += 1
-                print("Tab " + str(self.tab_counter) + " scraped")
-           
-            self.page_counter += 1
-            print("Page" + str(self.page_counter) + " scraped")
-            
-        except (TimeoutException, NoSuchElementException) as error:
-            print("finalizing due to error...")
-            self.driver.quit()
-            print(self.page_counter, self.tab_counter, self.row_counter)
-            raise
-            
-        print("Scraping successful")
-        self.driver.quit()
-        print(self.page_counter, self.tab_counter, self.row_counter)
-    
+driver = webdriver.Chrome("\webdrivers\chromedriver.exe")
+delay = 3
+driver.get(link)
+source = driver.page_source
+html = BeautifulSoup(source, "lxml")
 
-        
-compras_ar = BidScrape("https://comprar.gob.ar/BuscarAvanzado.aspx", 1, 12, 113)
-compras_ar.query_search()
-compras_ar.scrape()
+bid_code = driver.find_element_by_css_selector('span[id*="NumeroProceso"]').text
+name = driver.find_element_by_css_selector('span[id*="NombreProceso"]').text
+process = driver.find_element_by_css_selector('span[id*="ProcedimientoSeleccion"]').text
+stage = driver.find_element_by_css_selector('span[id*="Etapa"]').text
+validity = driver.find_element_by_css_selector('span[id*="MantenimientoOferta"]').text
+duration = driver.find_element_by_css_selector('span[id*="DuracionContrato"]').text
+opening = driver.find_element_by_css_selector('table[id*="ActasApertura"] p').text
+contract_info = driver.find_elements_by_css_selector('table[id*="DetalleImputacionAdjudicacion"] td')
+awarded_bidder = contract_info[1].text
+awarder_bidder_id = contract_info[2].text
+amount = contract_info[6].text
+currency = contract_info[7].text
+
+products = [product.text for product in driver.find_elements_by_css_selector('span[id*="lblDescripcion"]')]
+products_qty = [qty.text for qty in driver.find_elements_by_css_selector('span[id*="lblCantidad"]')]
+
+driver.quit()
